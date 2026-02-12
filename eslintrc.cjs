ebac4be3ee6262eaabb6453f071b67d5302805e5
codewithshoaib.dev <codewithshoaib.dev@gmail.com>
2026-02-12 21:35:02 +0500

    initial commit

---
+++ eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: ["eslint:recommended", "plugin:react-hooks/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off"
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-xl font-semibold">Ready.</h1>
+    </div>
+  );
+}
+
